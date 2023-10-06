bb9d14d2b4e5e5aca86acf91ec0393125b786995
Blockchain Developer <blockchaindeveloper@blockchaindev.local>
2023-10-06 12:18:14 +0100

    feat: My first feature

--- src/modules/HomePage/index.tsx
+++ src/modules/HomePage/index.tsx
@@ -5,11 +5,12 @@ const HomePage = () => {
   const { listedNFTs } = useNFTMarket();
   return (
     <div className="flex w-full flex-col">
-      <div className="flex flex-wrap">
+      {/* <div className="flex flex-wrap">
         {listedNFTs?.map((nft) => (
           <NFTCard nft={nft} className="mr-3 mb-3" key={nft?.id} />
         ))}
-      </div>
+      </div> */}
+      HERE
     </div>
   );
 };
