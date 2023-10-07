eda1d28f6484fb3968cc55a3e2ee36ed8c82c54c
Blockchain Developer <blockchaindeveloper@blockchaindev.local>
2023-10-07 14:35:54 +0100

    feat: My first feature

--- src/state/signer.tsx
+++ src/state/signer.tsx
@@ -26,8 +26,11 @@ export const SignerProvider = ({ children }: { children: ReactNode }) => {
 
   useEffect(() => {
     const web3modal = new Web3Modal();
-    if (web3modal.cachedProvider) connectWallet();
-    window.ethereum.on("accountsChanged", connectWallet);
+    if(window.ethereum) {
+      if (web3modal.cachedProvider) connectWallet();
+      window.ethereum.on("accountsChanged", connectWallet);
+    }
+
   }, []);
 
   const connectWallet = async () => {
